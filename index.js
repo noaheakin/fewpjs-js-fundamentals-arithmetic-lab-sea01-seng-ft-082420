2bc75b8be7d73e005d0f2ce35a21f372ab93a39c
noaheakin <noaheakin@gmail.com>
2021-01-18 14:32:01 -0800

    commit tree a0b68af71c5d07c222650c30e5214e3ebea75f18

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10)
+
+const mod = 20 % 8
+
+const max = Math.max(5, 15, 20, 10)
